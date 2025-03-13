bb005ad06418aa043c5f8dd6d427c34674b6038f
Santosh Passoubady <santoshpassoubady@gmail.com>
2025-03-13 13:03:15 -0400

    update

--- example/src/app/api/start-sso/route.js
+++ example/src/app/api/start-sso/route.js
@@ -3,14 +3,16 @@ import { RampartClient } from "@rampartcorporation/sdk";
 
 // Initialize Rampart Client
 const rampartClient = new RampartClient({
-  secret: 'sk-rvG7cx30dnuTbQMccDt5nYIXhvGeYtxf', // Your secret key
+  secret: process.env.RAMPART_SECRET, // Your secret key
+  environment: process.env.ENVIRONMENT,
 });
 
 export async function GET() {
   try {
+    console.log("RAMPART_SECRET", process.env.RAMPART_PARTNER_ID);
     // You would get this from your database
     const rampartPayload = {
-      iss: '019588b4-3bad-7605-8408-bdcd111df8da', // Your partner ID
+      iss: process.env.RAMPART_PARTNER_ID, // Your partner ID
       user: {
         id: "1234567890", // ID you associate with this user
         firstName: "John",
